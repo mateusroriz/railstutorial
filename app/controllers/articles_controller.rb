db0c583855a0083d236d21b3a769e9718ff71b32
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-18 22:28:07 -0300

    gerando um modelo para artigos, populando o banco de dados com uma entry e mostrando essa lista na pagina inicial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
+#controllers sao classes ruby e seus metodos publicos sao acoes
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 end
---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+#models sao classes usadas para representar dados elas podem interagiir com o db pelo Active record
+class Article < ApplicationRecord
+end
