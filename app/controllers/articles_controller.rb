bed516f224ef37ac0fb002b8685d923fa5933455
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-21 21:19:43 -0300

    adicionando metodos para criar um novo artigo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,28 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-def show
-  @article = Article.find(params[:id])
-end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new #instancia mas não salva
+    @article = Article.new
+  end
+
+  def create #instancia um novo article com valores para o title and body e tenta salvar
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end   
+  end
+
+  private 
+    def article_params #utilizando isso para permitir o envio dos parametros por hash
+     params.require(:article).permit(:title, :body)
+   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 #models sao classes usadas para representar dados elas podem interagir com o db pelo Active record
 class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :body, presence: true, length: {minimum: 10}
 end
