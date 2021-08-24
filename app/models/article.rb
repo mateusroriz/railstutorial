589ac13f03835a605cee0eab0a93aaafc2f60bb6
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-23 21:56:38 -0300

    adicionando model controller e view para comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 #models sao classes usadas para representar dados elas podem interagir com o db pelo Active record
 class Article < ApplicationRecord
+    has_many :comments
+
     validates :title, presence: true
     validates :body, presence: true, length: {minimum: 10}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do #cria comments como um nested resource dentro de articles
+    resources:comments 
+  end
 end
