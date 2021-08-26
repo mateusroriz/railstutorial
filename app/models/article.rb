41173f7d954c6781485c0d9573d51f7c78cd28cf
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-08-25 21:03:14 -0300

    adicionando metodo para deletar comentarios e deletando comentarios quando o artigo e deletado

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 #models sao classes usadas para representar dados elas podem interagir com o db pelo Active record
 class Article < ApplicationRecord
     include Visible
-    has_many :comments
+
+    has_many :comments, dependent: :destroy #deletando os dependentes no caso os comments
 
     validates :title, presence: true
     validates :body, presence: true, length: {minimum: 10}
